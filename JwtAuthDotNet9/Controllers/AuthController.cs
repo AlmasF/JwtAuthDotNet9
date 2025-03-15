17833120cffe03c406117ba7810dc0fedeb0de20
Almas Yoko_office <Almas almas@yoko.space>
2025-03-15 13:01:05 +0500

    feat: added AuthController, added User model and UserDto model

---
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            // code omitted for brevity
+            return Ok();
+        }
+    }
+}
+//https://youtu.be/6EEltKS8AwA?si=2Z54UCt-m4Jd_GFd&t=531
