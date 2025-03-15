e8dc78f98c7bb4a8116ca4a1d93e22ed18d41089
Almas Yoko_office <Almas almas@yoko.space>
2025-03-15 15:15:28 +0500

    Add authentication features and database setup
    - Implement login method in AuthController with JWT token generation.
    - Add Id property to User class for unique identification.
    - Update project dependencies for Entity Framework Core and JWT.
    - Configure UserDbContext for SQL Server in Program.cs.
    - Add connection string and JWT settings in appsettings.json.
    - Create UserDbContext for user entity management.
    - Add initial migration for Users table schema.
    - Introduce AuthService and IAuthService for authentication logic.

--- JwtAuthDotNet9/Controllers/AuthController.cs
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -3,12 +3,18 @@ using JwtAuthDotNet9.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace JwtAuthDotNet9.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user = new();
         [HttpPost("register")]
@@ -21,6 +27,82 @@ namespace JwtAuthDotNet9.Controllers
             user.PasswordHash = hashedPassword;
 
             return Ok(user);
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (
+                new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) 
+                == PasswordVerificationResult.Failed
+            )
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            // ===== new version ==================== probably
+            //var claims = new Dictionary<string, object>
+            //{
+            //    [ClaimTypes.Name] = user.Username,
+            //};
+
+
+            //var key = new SymmetricSecurityKey(
+            //    Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            //var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            //var tokenDescriptor = new SecurityTokenDescriptor
+            //{
+            //    Issuer = configuration.GetValue<string>("AppSettings:Issuer"),
+            //    Audience = configuration.GetValue<string>("AppSettings:Audience"),
+            //    Claims = claims,
+            //    IssuedAt = null,
+            //    Expires = DateTime.UtcNow.AddDays(1),
+            //    SigningCredentials = creds
+            //};
+
+            //var handler = new JsonWebTokenHandler();
+            //handler.SetDefaultTimesOnTokenCreation = false;
+            //var tokenString = handler.CreateToken(tokenDescriptor);
+
+            //return tokenString;
+
+            // ===== old version ====================
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken
+            (
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
---
+++ JwtAuthDotNet9/Service/AuthService.cs
@@ -0,0 +1,20 @@
+﻿using JwtAuthDotNet9.Data;
+using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+
+namespace JwtAuthDotNet9.Service
+{
+    public class AuthService(UserDbContext context, IConfiguration configuration) : IAuthService
+    {
+        public Task<string?> LoginAsync(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<User?> RegisterAsync(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+//https://youtu.be/6EEltKS8AwA?si=KWYtVYLmjIeOuWpq&t=2412
